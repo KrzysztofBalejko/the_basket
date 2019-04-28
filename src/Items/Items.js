64e11ed4588cc11d30db017b630a242b4eaedb37
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-04-28 22:17:14 +0100

    :recycle:  refactoring Items component

--- src/Items/Items.js
+++ src/Items/Items.js
@@ -1,53 +1,24 @@
 import React, { Component } from 'react'
-
+import classes from './Items.module.css'
 
 class Items extends Component {
 
     render() {
-
-        const liStyle = {
-            // display: 'flex',
-            // alignItems: 'stretch',
-
-            border: '1px solid black',
-            borderRadius: "5px",
-            padding: '10px',
-            margin: '10px',
-            marginLeft: '-30px',
-            width: '400px',
-            backgroundColor: '#EE9542',
-            
-        }
-
-        const btnStyle = {
-            border: '1px rgba(0, 0, 0, 0.74)',
-            backgroundColor: 'rgba(0, 0, 0, 0.74)',
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-            float: 'right',
-            borderRadius: '50%',
-            margin: '-7px'
-        }
-        
-        const xStyle = {
-            color: 'white',
-            fontSize: '28px',
-            padding: '3px'
-        }
  
         return (         
             this.props.basket.map((i) =>
-            <li className="selected" style={liStyle} key={i.id}>
+            <li className="selected" className={classes.liStyle} key={i.id}>
                 {i.item}
                 {i.price !== '' ? <span> £{i.price}</span> : null}
-                <button style={btnStyle} onClick={this.props.click.bind(this, i.id)} >
-                    <span style={xStyle}>
+                <button className={classes.btnStyle}
+                    onClick={this.props.click.bind(this, i.id)} >
+                    <span className={classes.xStyle}>
                         X
                     </span>
                 </button>
             </li>
         ))
 
-        
     }
 
 }
